6ff1fa9665e530a501c7cd3713c0f553f12b7dea
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-26 17:33:39 +0100

    dev: added css for table

--- front/src/stock/AppStock.tsx
+++ front/src/stock/AppStock.tsx
@@ -4,50 +4,52 @@ function AppStock() {
   return (
     <main className="home">
       <h1>Liste des articles</h1>
-      <nav>
-        <button>
-          <span className="icon-arrows-cw"></span>
-        </button>
-        <Link to="/stock/add">
+      <div className="content">
+        <nav>
           <button>
-            <span className="icon-plus"></span>
+            <span className="icon-arrows-cw"></span>
           </button>
-        </Link>
-        <button>
-          <span className="icon-trash"></span>
-        </button>
-      </nav>
-      <table>
-        <thead>
-          <tr>
-            <th className="name">Nom</th>
-            <th className="price">Prix</th>
-            <th className="qty">Quantité</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="name">Tournevis</td>
-            <td className="price">1.23 €</td>
-            <td className="qty">345</td>
-          </tr>
-          <tr>
-            <td className="name">Tournevis</td>
-            <td className="price">1.23 €</td>
-            <td className="qty">345</td>
-          </tr>
-          <tr>
-            <td className="name">Tournevis</td>
-            <td className="price">1.23 €</td>
-            <td className="qty">345</td>
-          </tr>
-          <tr>
-            <td className="name">Tournevis</td>
-            <td className="price">1.23 €</td>
-            <td className="qty">345</td>
-          </tr>
-        </tbody>
-      </table>
+          <Link to="/stock/add">
+            <button>
+              <span className="icon-plus"></span>
+            </button>
+          </Link>
+          <button>
+            <span className="icon-trash"></span>
+          </button>
+        </nav>
+        <table>
+          <thead>
+            <tr>
+              <th className="name">Nom</th>
+              <th className="price">Prix</th>
+              <th className="qty">Quantité</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="name">Tournevis</td>
+              <td className="price">1.23 €</td>
+              <td className="qty">345</td>
+            </tr>
+            <tr>
+              <td className="name">Tournevis</td>
+              <td className="price">1.23 €</td>
+              <td className="qty">345</td>
+            </tr>
+            <tr>
+              <td className="name">Tournevis</td>
+              <td className="price">1.23 €</td>
+              <td className="qty">345</td>
+            </tr>
+            <tr>
+              <td className="name">Tournevis</td>
+              <td className="price">1.23 €</td>
+              <td className="qty">345</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </main>
   );
 }
