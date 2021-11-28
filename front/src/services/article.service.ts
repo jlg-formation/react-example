b4d5201f09bcc3efdde4f9a8dd86add0b30abdec
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-28 15:38:27 +0100

    dev: adding add form

--- front/src/services/article.service.ts
+++ front/src/services/article.service.ts
@@ -1,10 +1,23 @@
 import { Observable, switchMap, timeout } from "rxjs";
 import { fromFetch } from "rxjs/fetch";
-import { Article } from "../interfaces/Article";
+import { Article, NewArticle } from "../interfaces/Article";
 
 const url = "http://localhost:3500/api/articles";
 
 class ArticleService {
+  add(article: NewArticle): Observable<void> {
+    return fromFetch(url, {
+      method: "POST",
+      body: JSON.stringify(article),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).pipe(
+      switchMap((response) => response.json()),
+      timeout(5000)
+    );
+  }
+
   get(): Observable<Article[]> {
     return fromFetch(url).pipe(
       switchMap((response) => response.json()),
--- front/src/stock/AppAdd.tsx
+++ front/src/stock/AppAdd.tsx
@@ -1,21 +1,82 @@
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  FormEvent,
+  FormEventHandler,
+  useState,
+} from "react";
+import { useNavigate } from "react-router-dom";
+import { lastValueFrom, timer } from "rxjs";
+import { NewArticle } from "../interfaces/Article";
+import { articleService } from "../services/article.service";
+
 function AppAdd() {
+  const [article, setArticle] = useState<NewArticle>({
+    name: "Tournevis",
+    qty: 10,
+    price: 1.23,
+  });
+
+  const [isAdding, setIsAdding] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (
+    event: FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    console.log("A name was submitted: ", article);
+    (async () => {
+      setIsAdding(true);
+      await lastValueFrom(articleService.add(article));
+      await lastValueFrom(timer(2000));
+      setIsAdding(false);
+      navigate("/stock");
+    })();
+  };
+
+  const handleChange: (
+    attr: keyof NewArticle
+  ) => ChangeEventHandler<HTMLInputElement> =
+    (attr: string) => (event: ChangeEvent<HTMLInputElement>) => {
+      const newArticle = { ...article, [attr]: event.target.value };
+      setArticle(newArticle);
+    };
+
   return (
     <main>
       <h1>Ajouter un article</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <div>Nom</div>
-          <input type="text" />
+          <input
+            type="text"
+            value={article.name}
+            onChange={handleChange("name")}
+          />
         </label>
         <label>
           <div>Prix</div>
-          <input type="text" />
+          <input
+            type="number"
+            value={article.price}
+            onChange={handleChange("price")}
+          />
         </label>
         <label>
           <div>Quantité</div>
-          <input type="text" />
+          <input
+            type="number"
+            value={article.qty}
+            onChange={handleChange("qty")}
+          />
         </label>
-        <button className="primary">Ajouter</button>
+        <button className="primary">
+          <span
+            className={isAdding ? "icon-spin5 animate-spin" : "icon-plus"}
+          ></span>
+          Ajouter
+        </button>
       </form>
     </main>
   );
